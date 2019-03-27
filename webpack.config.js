870c0c48e36e5c64b61b0e21129eabfd34876ea5
Divyansh Gothwal <shotsdivyansh@gmail.com>
2019-03-27 16:59:56 +0530

    Mychanges

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var cleanWebpackPlugin = require('clean-webpack-plugin');
+var BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
+var CompressionPlugin = require('compression-webpack-plugin');
 var extractPlugin = new ExtractTextPlugin({
    filename: 'main.css'
 });
@@ -10,12 +13,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist'
+        publicPath: './'
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: '/\.js$/',
                 use: [
                     {
                         loader: 'babel-loader',
@@ -30,10 +33,53 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            {
+                test:/\.html$/,
+                use:['html-loader']
+            },
+            {
+                test:/\.(jpg|png)$/,
+                use:[
+                    {
+                        loader:'file-loader',
+                        options:{
+                            name:'[name].[ext]',
+                            outputPath:'img/',
+                            publicPath:'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
-        extractPlugin
+        extractPlugin,
+        new htmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+        //new cleanWebpackPlugin({cleanOnceBeforeBuildPatterns:[path.resolve(__dirname, 'dist')]}),
+        new BrotliGzipPlugin({
+            asset: '[path].br[query]',
+            algorithm: 'brotli',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 1,
+            minRatio: 0.8,
+            quality: 11
+        }),
+        new BrotliGzipPlugin({
+            asset: '[path].br[query]',
+            algorithm: 'gzip',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+        // new CompressionPlugin({   
+        //     filename: "[path].gz[query]",
+        //     algorithm: "brotliCompress",
+        //     test: /\.js$|\.css$|\.html$/,
+        //     threshold: 1,
+        //     minRatio: 0.8
+        // })
     ]
 };
